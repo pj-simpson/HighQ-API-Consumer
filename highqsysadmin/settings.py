8335a581c8646286fe2253e01aa958d4ed2360ac
Peter Simpson <peter_joseph_simpson@hotmail.com>
2020-11-30 16:46:46 +0000

    better settings config

--- highqsysadmin/settings.py
+++ highqsysadmin/settings.py
@@ -25,26 +25,18 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = config("DEBUG", default=True, cast=bool)
 
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": config("PYTHONANYWHEREMYSQLENGINE",default="django.db.backends.sqlite3"),
+        "NAME": config("DBNAME", default=os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": config("DBUSER", default=""),
+        "PASSWORD": config("DBPASS", default=""),
+        "HOST": config("DBHOST", default=""),
     }
 }
-
-# ALLOWED_HOSTS = [
-#     "www.highqsysadmin.com",
-# ]
-#
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": config("PYTHONANYWHEREMYSQLDBNAME", default=""),
-#         "USER": config("PYTHONANYWHEREMYSQLDBUSER", default=""),
-#         "PASSWORD": config("PYTHONANYWHEREMYSQLDBPASS", default=""),
-#         "HOST": config("PYTHONANYWHEREMYSQLDBHOST", default=""),
-#     }
-# }
 
 THUMBNAIL_DEBUG = config("THUMBNAIL_DEBUG", default=True, cast=bool)
 
@@ -243,14 +235,14 @@ CRONJOBS = [
 CRONTAB_DJANGO_SETTINGS_MODULE = "highqsysadmin.settings.local"
 
 
-# if not DEBUG:
-#     SECURE_BROWSER_XSS_FILTER = True
-#     X_FRAME_OPTIONS = "DENY"
-#     SECURE_SSL_REDIRECT = True
-#     SECURE_HSTS_SECONDS = 3600
-#     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-#     SECURE_HSTS_PRELOAD = True
-#     SECURE_CONTENT_TYPE_NOSNIFF = True
-#     SESSION_COOKIE_SECURE = True
-#     CSRF_COOKIE_SECURE = True
-#     SECURE_REFERRER_POLICY = "same-origin"
+if not DEBUG:
+    SECURE_BROWSER_XSS_FILTER = True
+    X_FRAME_OPTIONS = "DENY"
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_REFERRER_POLICY = "same-origin"
